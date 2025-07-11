7f85a87a57afc79d14aa74276729b67fc0419019
goldoneryon <killian.parnaland2002@gmail.com>
2025-07-11 23:48:11 +0200

    Create getServers.js

---
+++ netlify/functions/getServers.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function () {
+  try {
+    const res = await fetch("https://throneandliberty.gameslantern.com/api/server-status");
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Erreur serveur" })
+    };
+  }
+};
